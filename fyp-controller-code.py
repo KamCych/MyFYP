0fec9d853b777d731423a1a9384c6f55e2396bfe
KamCych <cychowskik@gmail.com>
2025-12-01 15:49:01 +0000

    Added code for circuit + image

---
+++ fyp-controller-code.py
@@ -0,0 +1,28 @@
+from picozero import Pot, pico_led, Button
+
+from time import sleep
+
+pico_led.on()
+
+dial = Pot(26)
+butt = Button(15)
+
+def knob_pick():
+    if dial.value < 0.3:
+        print("PHONE HELP")
+        #turn on leftmost led
+    elif dial.value > 0.3 and dial.value < 0.6:
+        print("NET HELP")
+        #turn on middle led
+    else:
+        print("DISCONNECT")
+        #turn on rightmost led
+
+while True:
+    #print(dial.value)
+        
+    if butt.is_pressed:
+        print_pick()
+    
+    sleep(0.1)
+
